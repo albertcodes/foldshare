6e94230ab9027599bc6481b4cae23244377d11af
albertcodes <albertnthiwa@gmail.com>
2021-06-14 06:06:58 +0300

    Default components

---
+++ components/defaultComponent/bellMenu.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div v-if="bellMsg">
+      <div class="text-h5 grey--text text--darken-3 px-4 pt-4">
+        New Notifications
+      </div>
+      <div
+        class="subtitle-2 error--text font-weight-regular px-4 pb-2 link-item"
+      >
+        {{ bellMsg }} New Notifications
+      </div>
+    </div>
+    <!-- <v-list>
+      <v-list-item-group v-model="selectedItem">
+        <v-list-item
+          class="pd-5"
+          v-for="(bell, index) in bellList"
+          :key="index"
+        >
+          <v-list-item-icon class="mr-4 mb-1">
+            <v-btn icon :color="bell.color">
+              <v-icon>mdi-{{ bell.icon }}</v-icon>
+            </v-btn>
+          </v-list-item-icon>
+          <v-list-item-content style="width: 150px">
+            <v-list-item-title v-text="bell.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list> -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    bellMsg: {
+      type: Number,
+      default: 0,
+    },
+  },
+  // data() {
+  //   return {
+  //     selectedItem: '',
+  //     bellList: [
+  //       {
+  //         icon: 'thumb-up-outline',
+  //         title: 'Hey!How is it going?',
+  //         color: this.common.randomColor(),
+  //       },
+  //       {
+  //         icon: 'share-circle',
+  //         title: 'Hey!How is it going?',
+  //         color: this.common.randomColor(),
+  //       },
+  //       {
+  //         icon: 'heart-multiple',
+  //         title: 'Hey!How is it going?',
+  //         color: this.common.randomColor(),
+  //       },
+  //     ],
+  //   }
+  // },
+}
+</script>
+
+<style scoped>
+</style>
---
+++ components/defaultComponent/emailMenu.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <div v-if="emailMsg">
+      <div class="text-h5 grey--text text--darken-3 px-4 pt-4">
+        New Messages
+      </div>
+      <div
+        class="subtitle-2 primary--text font-weight-regular px-4 pb-2 link-item"
+      >
+        {{ emailMsg }} New Messages
+      </div>
+    </div>
+    <!-- <v-list>
+      <v-list-item-group v-model="selectedItem">
+        <v-list-item
+          class="pd-5"
+          v-for="(email, index) in emailList"
+          :key="index"
+        >
+          <v-list-item-icon class="mr-4 mb-1 d-flex flex-column">
+            <v-avatar :color="email.color" size="32">
+              <span class="white--text headline">{{ email.name }}</span>
+            </v-avatar>
+            <span>{{ email.time }}</span>
+          </v-list-item-icon>
+          <v-list-item-content style="width: 150px">
+            <v-list-item-title v-text="email.title"></v-list-item-title>
+            <v-list-item-subtitle v-text="email.description">
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list> -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    emailMsg: {
+      type: Number,
+      default: 0,
+    },
+  },
+  // data() {
+  //   return {
+  //     selectedItem: '',
+  //     emailList: [
+  //       {
+  //         name: 'JH',
+  //         title: 'Jane Hew',
+  //         description: 'Hey!How is it going?',
+  //         time: '09:32',
+  //         color: this.common.randomColor(),
+  //       },
+  //       {
+  //         name: 'JH',
+  //         title: 'Lloyd Brown',
+  //         description: 'Hey!How is it going?',
+  //         time: '09:32',
+  //         color: this.common.randomColor(),
+  //       },
+  //       {
+  //         name: 'JH',
+  //         title: 'Lloyd Brown',
+  //         description: 'Hey!How is it going?',
+  //         time: '09:32',
+  //         color: this.common.randomColor(),
+  //       },
+  //     ],
+  //   }
+  // },
+  // methods: {
+  //   randomColor() {
+  //     return '#' + Math.random().toString(16).slice(-6)
+  //   },
+  // },
+}
+</script>
+
+<style scoped>
+</style>
