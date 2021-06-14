4ac3993e51c4ff92b8adf7d91773966ee15964e6
albertcodes <albertnthiwa@gmail.com>
2021-06-14 06:10:00 +0300

    Vue components

---
+++ components/productCard.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-card
+    :to="{ path: '/dashboard', query: { url, title, subsTitle, money, star } }"
+  >
+    <v-img :src="url" height="200px">
+      <v-chip v-if="chip" class="ma-2" :color="chipColor"> {{ chip }} </v-chip>
+    </v-img>
+    <v-card-title>{{ title }}</v-card-title>
+
+    <v-card-subtitle> {{ subsTitle }} </v-card-subtitle>
+    <v-card-actions>
+      <v-btn color="orange lighten-2" text> ${{ money }} </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon style="width: 64px">
+        {{ star }}
+        <v-icon color="yellow">mdi-star</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      default: '',
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+    subsTitle: {
+      type: String,
+      default: '',
+    },
+    money: {
+      type: String,
+      default: '',
+    },
+    star: {
+      type: String,
+      default: '',
+    },
+    chip: {
+      type: String,
+      default: '',
+    },
+    chipColor: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      show: true,
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
---
+++ components/selfSelect.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-select
+    :value="values"
+    :items="items"
+    :label="label"
+    dense
+    outlined
+    @input="updateValue(values)"
+  ></v-select>
+</template>
+
+<script>
+export default {
+  props: {
+    values: {
+      type: String,
+      default: '',
+    },
+    items: {
+      type: Array,
+      default: [],
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    index: {
+      type: Number,
+      default: '',
+    },
+  },
+  methods: {
+    updateValue(val, index) {
+      this.$emit(val, index)
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
