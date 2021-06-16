31efe30204d7b752f7de33b3ab46345887db6a58
albertcodes <albertnthiwa@gmail.com>
2021-06-16 12:59:09 +0300

    updated profile component

--- components/profileComponent/pickerInfo.vue
+++ components/profileComponent/pickerInfo.vue
@@ -2,11 +2,11 @@
   <div class="mb-6 col col-12">
     <v-date-picker
       v-model="date"
-      :event-color="(date) => (date[9] % 2 ? 'red' : 'yellow')"
+      :event-color="(date) => (date[9] % 2 ? '#d9534f' : '#f0ad4e')"
       :events="functionEvents"
       full-width
       no-title
-      color="#e81607"
+      color="#5cb85c"
     ></v-date-picker>
   </div>
 </template>
@@ -22,7 +22,7 @@ export default {
     functionEvents(date) {
       const [, , day] = date.split('-')
       if ([12, 17, 28].includes(parseInt(day, 10))) return true
-      if ([1, 19, 22].includes(parseInt(day, 10))) return ['red', '#00f']
+      if ([1, 19, 22].includes(parseInt(day, 10))) return ['#d9534f', '#5bc0de']
       return false
     },
   },
