9027f9f60edabcec906fc3d958e6fe85dbc71bdf
albertcodes <albertnthiwa@gmail.com>
2021-06-16 12:57:36 +0300

    Updated components

---
+++ components/fileCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card
+    :to="{ path: '/dashboard', query: { url, title, subsTitle } }"
+  >
+    <v-img :src="url" height="200px">
+      <v-chip v-if="chip" class="ma-2" :color="chipColor"> {{ chip }} </v-chip>
+    </v-img>
+    <v-card-title>{{ title }}</v-card-title>
+
+    <v-card-subtitle> {{ subsTitle }} </v-card-subtitle>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      default: '',
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+    subsTitle: {
+      type: String,
+      default: '',
+    },
+    chip: {
+      type: String,
+      default: '',
+    },
+    chipColor: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      show: true,
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
