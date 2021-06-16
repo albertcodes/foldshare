a11b7f4a3bfa1a4249f8f17acafe3b59bba73699
albertcodes <albertnthiwa@gmail.com>
2021-06-16 13:00:47 +0300

    Updated default layout

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,14 +1,35 @@
 <template>
   <v-app dark>
+    <div class="col-12 d-flex flex-nowrap justify-space-between">
+      <div class="col-2"></div>
+      <div class="col-8">
+        <v-img src="404.png"> </v-img>
+      </div>
+      <div class="col-2"></div>
+    </div>
     <h1 v-if="error.statusCode === 404">
       {{ pageNotFound }}
     </h1>
     <h1 v-else>
       {{ otherError }}
     </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <!-- <NuxtLink to="/"> Home page </NuxtLink> -->
+    <div class="home col-12 d-flex flex-nowrap justify-space-between">
+      <div class="col-4"></div>
+      <div class="col-4 d-flex justify-center">
+        <v-btn
+          class="mt-4"
+          color="transparent"
+          type="button"
+          style="color: info"
+          href="/"
+        >
+          <v-icon right class="mr-2"> mdi-home </v-icon>
+          Back to safety
+        </v-btn>
+      </div>
+      <div class="col-4"></div>
+    </div>
   </v-app>
 </template>
 
@@ -18,27 +39,29 @@ export default {
   props: {
     error: {
       type: Object,
-      default: null
-    }
+      default: null,
+    },
   },
-  data () {
+  data() {
     return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
+      pageNotFound: 'Oops! Page Not Found',
+      otherError: 'An error occurred',
     }
   },
-  head () {
+  head() {
     const title =
       this.error.statusCode === 404 ? this.pageNotFound : this.otherError
     return {
-      title
+      title,
     }
-  }
+  },
 }
 </script>
 
 <style scoped>
 h1 {
-  font-size: 20px;
+  font-size: 24px;
+  color: yellow;
+  text-align: center;
 }
 </style>
