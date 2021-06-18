15294af02a0f0af0a987332a1e43fdf5d72d14bc
albertcodes <albertnthiwa@gmail.com>
2021-06-18 18:42:22 +0300

    Update pages

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -2,10 +2,12 @@
   <v-row>
     <v-col class="text-center">
       <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
+        src="/share.gif"
+        alt="share"
+        class="mb-5 rounded-lg"
+        height="400"
+        width="533"
+      />
       <blockquote class="blockquote">
         &#8220;Share files conviniently.&#8221;
         <footer>
@@ -13,6 +15,16 @@
             <em>&mdash;FoldShare Inc.</em>
           </small>
         </footer>
+        <v-btn
+          color="blue"
+          type="submit"
+          style="color: black"
+          href="/"
+          class="my-12"
+        >
+          <v-icon right class="mr-2"> mdi-arrow-left-circle </v-icon>
+          Back to FoldShare
+        </v-btn>
       </blockquote>
     </v-col>
   </v-row>
