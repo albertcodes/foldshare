74dbf4308270a2ae48508e7c9436397ae1780134
albertcodes <albertnthiwa@gmail.com>
2021-06-18 18:43:02 +0300

    Update pages

--- pages/user.vue
+++ pages/user.vue
@@ -3,52 +3,52 @@
     <div class="row">
       <div class="col col-12">
         <v-stepper non-linear class="transparent" v-model="el">
-          <v-card class="mb-5">
+          <v-card class="mb-5" >
             <v-stepper-header>
-              <v-stepper-step editable :step="1" :complete="el > 1">
+              <v-stepper-step editable :step="1" :complete="el > 1" color="pink">
                 Create Account
               </v-stepper-step>
 
               <v-divider></v-divider>
 
-              <v-stepper-step editable step="2" :complete="el > 2">
+              <v-stepper-step editable step="2" :complete="el > 2" color="pink">
                 User Details
               </v-stepper-step>
 
               <v-divider></v-divider>
 
-              <v-stepper-step step="3" :complete="el > 3" editable>
+              <v-stepper-step step="3" :complete="el > 3" editable color="pink">
                 Business Details
               </v-stepper-step>
               <v-divider></v-divider>
 
-              <v-stepper-step step="4" editable> Social </v-stepper-step>
+              <v-stepper-step step="4" editable color="pink"> Social </v-stepper-step>
             </v-stepper-header>
           </v-card>
           <v-card>
             <v-stepper-items>
               <v-stepper-content step="1">
-                <Craete>
-                  <v-btn color="primary" @click="next"> Next </v-btn>
-                </Craete>
+                <Create>
+                  <v-btn color="success" @click="next"> Next </v-btn>
+                </Create>
               </v-stepper-content>
               <v-stepper-content step="2">
                 <Detail>
-                  <v-btn outlined color="primary" @click="back"> Back </v-btn>
-                  <v-btn color="primary" @click="next"> Next </v-btn>
+                  <v-btn outlined color="pink" @click="back"> Back </v-btn>
+                  <v-btn color="success" @click="next"> Next </v-btn>
                 </Detail>
               </v-stepper-content>
               <v-stepper-content step="3">
                 <Business>
-                  <v-btn outlined color="primary" @click="back"> Back </v-btn>
-                  <v-btn color="primary" @click="next"> Next </v-btn>
+                  <v-btn outlined color="pink" @click="back"> Back </v-btn>
+                  <v-btn color="success" @click="next"> Next </v-btn>
                 </Business>
               </v-stepper-content>
               <v-stepper-content step="4">
                 <Social>
-                  <v-btn outlined color="primary" @click="back"> Back </v-btn>
+                  <v-btn outlined color="pink" @click="back"> Back </v-btn>
                   <v-btn
-                    color="primary"
+                    color="success"
                     @click="finish"
                     :loading="finishLoading"
                   >
@@ -65,14 +65,13 @@
 </template>
 
 <script>
-// const Craete = import('../components/userComponent/create')
-import Craete from '../components/userComponent/create'
+import Create from '../components/userComponent/create'
 import Detail from '../components/userComponent/detail'
 import Business from '../components/userComponent/business'
 import Social from '../components/userComponent/social'
 export default {
   components: {
-    Craete,
+    Create,
     Detail,
     Business,
     Social,
@@ -94,7 +93,7 @@ export default {
       this.finishLoading = true
       setTimeout(() => {
         this.finishLoading = false
-        this.$router.push('/documentation')
+        this.$router.push('/')
       }, 2000)
     },
   },
